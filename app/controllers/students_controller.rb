56a7c27395268b2871a85dfe76c8f18bc95decd0
itokanta <80323505+itokanta@users.noreply.github.com>
2021-06-23 09:59:21 +0900

    コントローラーの記述

--- app/controllers/students_controller.rb
+++ app/controllers/students_controller.rb
@@ -7,6 +7,17 @@ class StudentsController < ApplicationController
   end
 
   def create
+    @student = Student.new(student_params)
+    if @student.save
+      redirect_to root_path
+    else
+      render :new
+    end
+  end
+
+  private
+  def student_params
+    params.require(:student).permit(:name, :age, :guardian_name, :phone_number).merge(user_id: current_user.id)
   end
 
 end
