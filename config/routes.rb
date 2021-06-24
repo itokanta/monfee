004e1dec129c2eec0bf07dd9d50305a5bec19717
itokanta <80323505+itokanta@users.noreply.github.com>
2021-06-24 11:07:27 +0900

    ルーティングの設定

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,5 @@
 Rails.application.routes.draw do
   devise_for :users
   root to: "students#index"
-  resources :students, only: [:new, :create, :edit]
+  resources :students, only: [:new, :create, :edit, :update]
 end
