0278bb2edcdf499da35ab58348b4761b493fc79d
itokanta <80323505+itokanta@users.noreply.github.com>
2021-06-28 11:19:53 +0900

    出席・料金登録機能実装

--- app/controllers/attendances_controller.rb
+++ app/controllers/attendances_controller.rb
@@ -1,4 +1,28 @@
 class AttendancesController < ApplicationController
+  before_action :student_choose, except:[:index]
+
   def index
+  end
+
+  def new
+    @attendance = Attendance.new
+  end
+
+  def create
+    @attendance = Attendance.new(attendance_params)
+    if @attendance.save
+      redirect_to student_attendances_path(@student)
+    else
+      render :new
+    end
+  end
+
+  private
+  def attendance_params
+    params.require(:attendance).permit(:entry, :fee).merge(student_id: params[:student_id])
+  end
+
+  def student_choose
+    @student = Student.find(params[:student_id])
   end
 end
