f8725280207fc71a849f49cf29bbb4d0ce6a3cd4
itokanta <80323505+itokanta@users.noreply.github.com>
2021-06-29 14:53:14 +0900

    コントローラー修正

--- app/controllers/students_controller.rb
+++ app/controllers/students_controller.rb
@@ -1,5 +1,5 @@
 class StudentsController < ApplicationController
-  before_action :authenticate_user!
+  before_action :authenticate_user!, except:[:index]
   before_action :student_choose, except:[:index, :new, :create]
   
   def index
