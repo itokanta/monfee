437b48805caaea5c31befd6ae3293295ed96b3bd
itokanta <80323505+itokanta@users.noreply.github.com>
2025-07-27 23:10:12 +0900

    attendancesコントローラーの修正
    new、editメソッドに、FeePlanモデルからプランを取得する記述を追加
    create、updateメソッドに、FeePlanモデルから取得したプランをfeeに設定する記述を追加
    attendance_paramsメソッドのストロングパラメーターにfee_plan_idを追加

--- app/controllers/attendances_controller.rb
+++ app/controllers/attendances_controller.rb
@@ -14,36 +14,53 @@ class AttendancesController < ApplicationController
 
   def new
     @attendance = Attendance.new
+    @fee_plans = FeePlan.all
   end
 
   def create
     @attendance = Attendance.new(attendance_params)
+    
+    # fee_planが選択されている場合、そのプランの金額をfeeに設定
+    if params[:attendance][:fee_plan_id].present?
+      fee_plan = FeePlan.find(params[:attendance][:fee_plan_id])
+      @attendance.fee = fee_plan.amount
+    end
+    
     if @attendance.save
-      redirect_to student_attendances_path(@student)
+      redirect_to student_path(@student)
     else
+      @fee_plans = FeePlan.all
       render :new
     end
   end
 
   def edit
+    @fee_plans = FeePlan.all
   end
 
   def update
+    # fee_planが選択されている場合、そのプランの金額をfeeに設定
+    if params[:attendance][:fee_plan_id].present?
+      fee_plan = FeePlan.find(params[:attendance][:fee_plan_id])
+      @attendance.fee = fee_plan.amount
+    end
+    
     if @attendance.update(attendance_params)
-      redirect_to student_attendances_path(@student)
+      redirect_to student_path(@student)
     else
-      render :new
+      @fee_plans = FeePlan.all
+      render :edit
     end
   end
 
   def destroy
     @attendance.destroy
-    redirect_to student_attendances_path(@student)
+    redirect_to student_path(@student)
   end
 
   private
   def attendance_params
-    params.require(:attendance).permit(:entry, :fee).merge(student_id: params[:student_id])
+    params.require(:attendance).permit(:entry, :fee, :fee_plan_id).merge(student_id: params[:student_id])
   end
 
   def student_entry
