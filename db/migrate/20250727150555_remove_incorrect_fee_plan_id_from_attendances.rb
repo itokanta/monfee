9e94ff09994d6a507db4b1f6bcac521a90a99820
itokanta <80323505+itokanta@users.noreply.github.com>
2025-08-13 23:11:55 +0900

    不要なマイグレーションファイルの削除

--- db/migrate/20250727150555_remove_incorrect_fee_plan_id_from_attendances.rb
+++
@@ -1,6 +0,0 @@
-class RemoveIncorrectFeePlanIdFromAttendances < ActiveRecord::Migration[6.0]
-  def change
-    remove_index :attendances, :fee_plan_id_id if index_exists?(:attendances, :fee_plan_id_id)
-    remove_column :attendances, :fee_plan_id_id, :bigint
-  end
-end
