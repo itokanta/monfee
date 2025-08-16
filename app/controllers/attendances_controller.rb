34da14f1db4c6d6fa48cc7e1a29a2a6a7a14a419
itokanta <80323505+itokanta@users.noreply.github.com>
2025-08-16 23:40:19 +0900

    出席登録のバリデーションエラー表示とUX改善
    - フォーム上部に日本語エラーメッセージを統一表示
    - ブラウザバリデーションを無効化してサーバーサイドに統一
    - エラー時のURL維持 (attendances/new) をセッション管理で実装
    - 料金フィールドのバリデーションを削除（自動設定のため不要）
    - AttendancesControllerをリファクタリングして重複コードを削除
    
    変更ファイル:
    - app/views/attendances/_form.html.erb
    - app/controllers/attendances_controller.rb
    - app/models/attendance.rb
    - config/locales/ja.yml

--- app/controllers/attendances_controller.rb
+++ app/controllers/attendances_controller.rb
@@ -8,31 +8,21 @@ class AttendancesController < ApplicationController
 
   def new
     @attendance = Attendance.new
+    restore_from_session(@attendance)
   end
 
   def create
     @attendance = Attendance.new(attendance_params)
-    
-    return unless set_fee_from_plan(@attendance)
-    
-    if @attendance.save
-      redirect_to student_path(@student)
-    else
-      render :new
-    end
+    save_attendance_with_error_handling { new_student_attendance_path(@student) }
   end
 
   def edit
+    restore_from_session(@attendance)
   end
 
   def update
-    return unless set_fee_from_plan(@attendance)
-    
-    if @attendance.update(attendance_params)
-      redirect_to student_path(@student)
-    else
-      render :edit
-    end
+    @attendance.assign_attributes(attendance_params)
+    save_attendance_with_error_handling { edit_student_attendance_path(@student, @attendance) }
   end
 
   def destroy
@@ -55,11 +45,6 @@ class AttendancesController < ApplicationController
       attendance.fee = fee_plan.amount
       attendance.fee_plan = fee_plan
       # fee_plan_nameはモデルのbefore_saveコールバックで自動設定される
-      return true
-    else
-      attendance.errors.add(:fee_plan_id, 'を選択してください')
-      render action_name == 'create' ? :new : :edit
-      return false
     end
   end
   
@@ -74,5 +59,38 @@ class AttendancesController < ApplicationController
 
   def attendance_choose
     @attendance = Attendance.find(params[:id])
+  end
+
+  # セッションからエラー情報と入力値を復元
+  def restore_from_session(attendance)
+    # セッションからエラー情報を復元
+    if session[:attendance_errors].present?
+      attendance.errors.add(:base, '') # エラーオブジェクトを初期化
+      session[:attendance_errors].each do |field, messages|
+        messages.each { |message| attendance.errors.add(field.to_sym, message) }
+      end
+      session.delete(:attendance_errors)
+    end
+    
+    # セッションから入力値を復元
+    if session[:attendance_params].present?
+      attendance.assign_attributes(session[:attendance_params])
+      session.delete(:attendance_params)
+    end
+  end
+
+  # 出席記録の保存とエラーハンドリング
+  def save_attendance_with_error_handling(&redirect_path_block)
+    set_fee_from_plan(@attendance)
+    
+    if @attendance.save
+      redirect_to student_path(@student)
+    else
+      # エラー情報をセッションに保存
+      session[:attendance_errors] = @attendance.errors.messages
+      session[:attendance_params] = attendance_params.except(:student_id)
+      
+      redirect_to redirect_path_block.call
+    end
   end
 end
